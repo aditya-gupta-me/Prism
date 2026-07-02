60e1adc6c5a75f028c0e30a7375970c37916443e
aditya-gupta-me <adityagupta46772@gmail.com>
2026-07-02 20:56:22 +0530

    feat: integrate GitHub OAuth & async repository import/export pipeline

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,10 +1,13 @@
 // src/app/api/inngest/route.ts
 import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
-import { demoGenerate } from "../../../inngest/functions";
-import { processMessage } from "@/features/conversations/inngest/process-message";
 
+import { inngest } from "@/inngest/client";
+import { processMessage } from "@/features/conversations/inngest/process-message";
+import { importGithubRepo } from "@/features/projects/inngest/import-github-repo";
+import { exportToGithub } from "@/features/projects/inngest/export-to-github";
+
+// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [demoGenerate, processMessage],
+  functions: [processMessage, importGithubRepo, exportToGithub],
 });
